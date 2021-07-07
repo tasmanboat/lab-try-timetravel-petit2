5b8faedc2a01c9905a170973c6c99f76ae2a1f00
noname <you@example.com>
2021-07-07 20:20:01 +0800

    modify SessionController.php

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -41,10 +41,9 @@ class SessionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy()
     {
         Auth::logout();
         session()->flash('success', 'Logged out');
